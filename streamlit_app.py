c2ec3072c1a9889af4093adf2f98544feefec1d0
jmwork4 <148482660+jmwork4@users.noreply.github.com>
2023-10-19 14:29:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
-streamlit.body ('Omega 3 & Blueberry Oatmeal')
+streamlit.write ('Omega 3 & Blueberry Oatmeal')
