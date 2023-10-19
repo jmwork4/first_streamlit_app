5fe477e005240ddea8d9a918e70fa3186b1116b6
jmwork4 <148482660+jmwork4@users.noreply.github.com>
2023-10-19 14:34:09 -0400

    Update streamlit_app.py
    Updated menu items to include all three items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
-menu_items = [
+streamlit.write(
     'Omega 3 & Blueberry Oatmeal',
     'Kale, Spinach & Rocket Smoothie',
     'Hard-Boiled Free Range Egg'
-]
-
-streamlit.write(menu_items)
+)
 
 
 
