a060a80aadb68d576af044ae30a79e8b0adee7db
jmwork4 <148482660+jmwork4@users.noreply.github.com>
2023-10-26 20:34:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
 else:
@@ -57,7 +57,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays the normalized data - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-streamlit.error
+streamlit.error()
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
